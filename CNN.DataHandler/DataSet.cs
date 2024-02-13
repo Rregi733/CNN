b81c4813d7588334efce07c13352b3347d861652
Rregi733 <45148441+Rregi733@users.noreply.github.com>
2024-02-13 23:07:00 +0200

    13 Feb 24

--- CNN.DataHandler/DataSet.cs
+++ CNN.DataHandler/DataSet.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CNN.DataHandler
 {
     internal class DataSet
     {
-        private string path;
+        private string baseDir;
+        private string trainPath;
+        private string testPath;
+        private string fineLabelPath;
+        
+
         public DataSet(string path) 
         { 
-            this.path = path;
+            this.baseDir = path;
+            this.trainPath = Path.GetFullPath(baseDir + "\\train.bin");
+            this.testPath = Path.GetFullPath(baseDir + "\\test.bin");
+            this.fineLabelPath = Path.GetFullPath(baseDir + "\\fine_label_names.txt");
+            byte[] bytesTrain = File.ReadAllBytes(trainPath);
+
+            byte[] bytesTest = File.ReadAllBytes(testPath);
+            System.Console.WriteLine(bytesTest[0]); System.Console.WriteLine(bytesTest[1]);
+            for (int i =  2; i < 3074; i++) 
+            { System.Console.Write(bytesTest[i]);}
+
+            string[] fineLabels = File.ReadAllLines(fineLabelPath);
+            System.Console.WriteLine(fineLabels[1]);
         }
 
     }
--- CNN.DataHandler/Program.cs
+++ CNN.DataHandler/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informationž
+using System;
+using CNN.DataHandler;
+
+ DataSet dataSet = new DataSet("C:\\cifar-100-binary");
+
+
